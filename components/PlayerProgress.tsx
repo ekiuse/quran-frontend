6f82bfc181ad3848abce29ccd1262f7e88be48b2
ekiuse <0ekiuse1@gmail.com>
2026-07-01 14:31:52 +0330

    delete nextjs

--- components/PlayerProgress.tsx
+++
@@ -1,9 +0,0 @@
-"use client";
-import { usePlayOptions } from "@/contexts/playOptionsContext";
-import { Progress } from "./ui/progress";
-
-export default function PlayerProgress() {
-    const [playOptions] = usePlayOptions();
-
-    return <Progress value={playOptions.progress} className="h-0.5" />;
-}
